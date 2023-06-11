66eddcbf41e51876bbad08543e22ea5a4060522c
fazlerabbyrifat <rifatfazlerabby@gmail.com>
2023-06-12 02:56:40 +0600

    get popular instructors data

--- index.js
+++ index.js
@@ -45,6 +45,12 @@ async function run() {
         res.send(instructors);
     })
 
+    // popular instructors api
+    app.get('/popular-instructors', async(req, res) => {
+        const popularInstructors = await instructorsCollection.find().sort({total_students: -1}).limit(6).toArray();
+        res.send(popularInstructors);
+    })
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
